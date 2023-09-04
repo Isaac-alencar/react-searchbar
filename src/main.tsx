e47c10622e74fc24ba635dcc75e0b5a7c8151382
Isaac Alencar <alencarisaac3@gmail.com>
2023-09-04 16:41:31 -0300

    fix: fixed error related with transient props

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
 import { GlobalStyles } from "./styles/globalStyles.ts";
+import { StyleSheetManager } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <StyleSheetManager shouldForwardProp={isPropValid}>
+      <GlobalStyles />
+      <App />
+    </StyleSheetManager>
   </React.StrictMode>
 );
